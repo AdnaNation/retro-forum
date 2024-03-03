346770aeadf97297fe5935c6225699dee1ba2a83
AdnaNation <azizulislamadnan@gmail.com>
2024-03-03 16:41:36 +0600

    selected forums container added

--- forum.js
+++ forum.js
@@ -2,12 +2,12 @@ const loadForums = async () =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
     const data = await res.json();
     const forums = data.posts;
-    //  console.log(forums);
+    //  
 
     const forumsContainer = document.getElementById('forums-container');
     forums.forEach(forum =>{
         const div = document.createElement('div');
-        // console.log(forum)
+        //  console.log(forum)
         div.className = 'flex gap-3 bg-gray-100 p-8 border border-dashed rounded-xl max-w-[700px] shadow-xl'
         
         div.innerHTML = `
@@ -21,7 +21,7 @@ const loadForums = async () =>{
                     <span class="mr-3 ">#${forum.category}</span> <span>Author: ${forum.author?.name}</span>
                 </p>
                 </div>
-                <h4 class="text-2xl font-bold text-[#12132D]">${forum.title} </h4>
+                <h4 class="forum-title text-2xl font-bold text-[#12132D]">${forum.title} </h4>
                 <p class="text-xl text-[#12132D99]">${forum.description}</p>
                 <hr class="border border-dotted">
         <div class="flex justify-between">
@@ -41,7 +41,7 @@ const loadForums = async () =>{
                 </span>
             </div>
 
-            <button onclick="" class="btn-clicked"><img src="images/email.png" alt=""></button>
+            <button onclick="buttonClick('${forum.title}', '${forum.view_count}')" class="btn-clicked"><img src="images/email.png" alt=""></button>
 
          </div>
             </div>
@@ -49,17 +49,30 @@ const loadForums = async () =>{
         `;
         forumsContainer.appendChild(div);
 
-        const buttons = document.querySelectorAll('.btn-clicked');
-        for(const btn of buttons){
-            btn.addEventListener('click', function(){
-                 
-                 console.log('clicked')
-            })
-        }
       
-        
-     
-    })
+    });
+
+};
+const selectedForumsContainer= document.getElementById('selected-forums-container');
+const div = document.createElement('div');
+div.innerText= 'I love'
+selectedForumsContainer.appendChild(div)
+
+
+const buttonClick = (title,view) =>{
+    // const selectedForumsContainer= document.getElementById('selected-forums-container');
+    const div = document.createElement('div');
+    const div2 = document.createElement('div')
+    const div3 = document.createElement('div')
+    const div4 = document.createElement('div')
+    div2.innerText = title;
+    div3.innerText =view;
+    div4.innerHTML= ` <img class="ml-24" src="images/Vector (1).png" alt="">`
+    div.appendChild(div2);
+    div.appendChild(div4);
+    div.appendChild(div3);
+    selectedForumsContainer.appendChild(div)
+    div.className='flex gap-8 items-center bg-white rounded-lg my-5 p-4'
 
 }
 
